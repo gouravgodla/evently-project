b1ca8d5cd3478045f21651280ef5552492d99fe8
gouravgodla <98866998+gouravgodla@users.noreply.github.com>
2025-03-27 23:25:45 +0530

    fixing error from update 1.1 #11
    dropdown problem solve text input problem

---
+++ lib/validator.ts
@@ -0,0 +1,15 @@
+import { title } from "process"
+import { start } from "repl"
+import { z } from "zod"
+export const eventFormSchema = z.object({
+    title: z.string().min(3, 'Title must be at least 3 characters'),
+    description: z.string().min(3,'Description must be at least 3 characters').max(400 ,'Description must be less than 400 characters'),
+    location: z.string().min(3, 'Location must be at least 3 characters').max(400, 'Location must be less than 400 characters'),
+    imageUrl: z.string(),
+    startDateTime: z.date(),
+    endDateTime: z.string(),
+    price: z.string(),
+    isFree: z.boolean(),
+    url: z.string().url(),
+    categoryId: z.string().min(1, "Category is required"), // Added categoryId
+  })
