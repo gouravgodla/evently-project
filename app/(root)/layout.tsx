9fb0f030ecb6e5ea942332103467313cc42823c7
gouravgodla <98866998+gouravgodla@users.noreply.github.com>
2025-04-03 22:21:35 +0530

    vercel update

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,16 +1,18 @@
-import Footer from "@/components/shared/Footer"
-import Header from "@/components/shared/Header"
+import Footer from "@/components/shared/Footer";
+import Header from "@/components/shared/Header";
+import { Analytics } from "@vercel/analytics/next";
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <div className="flex h-screen flex-col">
       <Header />
       <main className="flex-1">{children}</main>
       <Footer />
+      <Analytics />
     </div>
-  )
+  );
 }
